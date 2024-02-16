e817fc5a8ddd31524636a722262c366b196ab062
Ptriciax <148201846+Ptriciax@users.noreply.github.com>
2024-02-17 01:45:28 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,61 +1,28 @@
-LetmenuIcon = document.querySelector('#menu-icon');
-Letnavbar = document.querySelector('.navbar');
-
-Letsections = document.querySelectorAll('section');
-LetnavLinks= document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx bx-x');
-    navbar.classList.toggle('active');
-
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
 }
 
 window.onscroll = () => {
     sections.forEach(sec => {
-        Lettop = window.scrollY;
-        Letoffset = sec.offsetTop - 150;
-        Letheight = sec.offsetHeight;
-        Letid = sec.getAttribute('id');
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if(top = offset && top < offset + height) {
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
             });
-        };
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
     });
 
-    Letheader = document.querySelector('header');
-
-    Headers.classList.toggle('sticky', window.scrollY > 100);
-};LetmenuIcon = document.querySelector('#menu-icon');
-Letnavbar = document.querySelector('.navbar');
-
-Letsections = document.querySelectorAll('section');
-LetnavLinks= document.querySelectorAll('header nav a');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx bx-x');
-    navbar.classList.toggle('active');
-
-}
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        Lettop = window.scrollY;
-        Letoffset = sec.offsetTop - 150;
-        Letheight = sec.offsetHeight;
-        Letid = sec.getAttribute('id');
-
-        if(top = offset && top < offset + height) {
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-
-    Letheader = document.querySelector('header');
-
-    Headers.classList.toggle('sticky', window.scrollY > 100);
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 };
